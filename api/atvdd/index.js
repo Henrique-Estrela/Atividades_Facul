74f3bcd9822edb3c6ce78ca0107ffea1b1819271
Henrique Estrela <92654861+Henrique-Estrela@users.noreply.github.com>
2024-10-14 21:15:59 -0300

    up

---
+++ api/atvdd/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+
+var app = express();
+
+app.use(express.json()); 
+
+app.get('/ola-nome', async (req, res) => {
+    try{
+        res.status(200).send("Hello, World!");
+    }
+    catch(error){
+        res.status(400).json({ message: error.message });
+    }
+});
+
+
+app.post('/ola-nome/', async (req, res) => {
+    try{
+        const nome = req.body.nome; // Extraindo nome e email do corpo da requisição
+        res.status(200).send("\nNome: " + nome); // Retornando apenas o nome
+    }
+    catch(error){
+        res.status(500).json({ message: error.message });
+    }
+});
+
+
+app.listen(3000, function(){
+    console.log("Servidor iniciado!")
+})
+
+
+
